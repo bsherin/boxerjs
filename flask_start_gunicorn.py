dfe028da8e620cb33e31472723cf0ed88b4b18b0
bsherin <bsherin@northwestern.edu>
2020-02-16 10:22:39 -0600

    working no getting running on server

---
+++ flask_start_gunicorn.py
@@ -0,0 +1,11 @@
+
+
+from boxer_app import app, socketio
+print("back in host_main")
+import users
+print("imported user")
+import auth_views, main_views, library_views, admin_views
+
+
+# socketio.run(app, port=8000)
+
