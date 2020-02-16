7c62acbdbc88a61257071456a6e455fb146668fd
bsherin <bsherin@northwestern.edu>
2020-02-16 09:19:44 -0600

    now with the library machinery

--- flask_start.py
+++ flask_start.py
@@ -7,35 +7,5 @@ print("imported user")
 import auth_views, main_views, library_views, admin_views
 
 
-socketio.run(app)
+socketio.run(app, port=8000)
 
-dstruct = {"name": "world",
-           "kind": "databox",
-           "line_list": [{"kind": "line",
-                          "node_list": [{"name": "mybox",
-                                         "kind": "databox",
-                                         "line_list": [{"kind": "line",
-                                                        "node_list": [{"name": "mb2",
-                                                                       "kind": "text",
-                                                                       "the_text": "some simple text"}]},
-                                                       {"kind": "line",
-                                                        "node_list": [{"name": "mb3",
-                                                                       "kind": "text",
-                                                                       "the_text": "with newline"},
-                                                                      {"name": "innerbox",
-                                                                       "kind": "databox",
-                                                                       "line_list": [{"kind": "line",
-                                                                                      "node_list": [{"kind": "text",
-                                                                                                     "the_text": "some inner text with a newline"}]}
-                                                                                     ]},
-                                                                      {"kind": "text",
-                                                                       "the_text": "more text"}]}]},
-                                        {"name": "mybox2",
-                                         "kind": "databox",
-                                         "line_list": [{"kind": "line",
-                                                        "node_list": [{"kind": "text",
-                                                                       "the_text": "more simple text"}]}]}
-                                        ]
-                          }
-                         ]
-           }
